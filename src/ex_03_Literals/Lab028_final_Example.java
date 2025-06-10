9937e0e0efda53d9a84a42be6e3d0996de7802f9
HarjinderK123 <soni.saini34@gmail.com>
2025-06-10 23:11:23 +0530

    9th June 2025 different type of class name

---
+++ src/ex_03_Literals/Lab028_final_Example.java
@@ -0,0 +1,10 @@
+package ex_03_Literals;
+
+public class Lab028_final_Example {
+    public static void main(String[] args) {
+        final float PI = 3.14f;
+        //PI = 3.13F, This is final in nature
+        System.out.println(PI);
+
+    }
+}
