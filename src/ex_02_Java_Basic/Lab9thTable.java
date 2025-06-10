39e1d925d146cac637edea90701dda97e135118f
HarjinderK123 <soni.saini34@gmail.com>
2025-06-10 23:30:39 +0530

    9th June 2025 different type of class name

---
+++ src/ex_02_Java_Basic/Lab9thTable.java
@@ -0,0 +1,29 @@
+package ex_02_Java_Basic;
+
+public class Lab9thTable {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 1;
+        //**System.out.printf("9 * 1 = %d\n",a,b,a*b) ;
+        //System.out.printf("9 * 1 = %d\n",a,++b,a*b) ;
+        //System.out.printf("9 * 1 = %d\n",a,++b,a*b) ;
+        //System.out.printf("9 * 1 = %d\n",a,++b,a*b) ;
+        //System.out.printf("9 * 1 = %d\n",a,++b,a*b) ;
+
+        System.out.printf("9 * 1 = %d\n",9*1) ;
+        System.out.printf("9 * 2 = %d\n",9*2) ;
+        System.out.printf("9 * 3 = %d\n",9*3) ;
+        System.out.printf("9 * 4 = %d\n",9*4) ;
+        System.out.printf("9 * 5 = %d\n",9*5) ;
+        System.out.printf("9 * 6 = %d\n",9*6) ;
+        System.out.printf("9 * 7 = %d\n",9*7) ;
+        System.out.printf("9 * 8 = %d\n",9*8) ;
+        System.out.printf("9 * 9 = %d\n",9*9) ;
+        System.out.printf("9 * 10 = %d\n",9*10) ;
+
+
+
+
+
+    }
+}
