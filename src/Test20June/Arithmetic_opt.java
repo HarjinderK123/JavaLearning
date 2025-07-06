9b7aab7b783a203b0341dc7b43edcbbd2303836b
HarjinderK123 <soni.saini34@gmail.com>
2025-07-06 16:15:11 +0530

    9th June 2025 different type of class name

---
+++ src/Test20June/Arithmetic_opt.java
@@ -0,0 +1,20 @@
+package Test20June;
+
+public class Arithmetic_opt {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+//arthmatical operation
+        int sum = a+b;
+        int sub = a-b;
+        int product = a*b;
+        int quotient = a /b;
+        int remainder = a % b;
+        System.out.println("Value of a+b :" + sum );
+        System.out.println("Value of a*b :" + product);
+        System.out.println("Value of a/b :" + quotient );
+        System.out.println("Value of a-b :" + sub );
+        System.out.println("Value of a%b :" + remainder );
+    }
+
+}
